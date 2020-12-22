f0eed01b297c8d9d33b094cd2cf35168820485e4
Arman Moztarzadeh <75223820+snoo8910@users.noreply.github.com>
2020-12-22 10:44:20 -0800

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -6,6 +6,6 @@ session.login()
 session.set_relationship_bounds(enabled = True, max_followers = 200)
 
 session.set_do_follow(True, percentage=100)
-session.like_by_tags(["3d printing", "animation"])
+session.like_by_tags(["INTSERT_TAG"])
 
 session.end()
