6dba73ba48fbf4984032eaae5afe39ffc4c6f8ba
Arman Moztarzadeh <75223820+snoo8910@users.noreply.github.com>
2020-12-22 10:40:16 -0800

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,1 +1,11 @@
+from instapy import InstaPy
 
+session = InstaPy(username = '3baprinting', password = 'poggers123')
+session.login()
+
+session.set_relationship_bounds(enabled = True, max_followers = 200)
+
+session.set_do_follow(True, percentage=100)
+session.like_by_tags(["3d printing", "animation"])
+
+session.end()
